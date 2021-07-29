d5992d242c1e4ff4e40687b39c590b600de161da
Panthonf <76035497+Panthonf@users.noreply.github.com>
2021-07-29 14:22:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ const color = document.querySelector('#color')
 // define more constants and variables here
 
 btn_toggle.onclick = () => {
-  // your code here
+    btn_toggle.classList.toggle('button');
+    author.innerHTML = '640631102'
+    btn_toggle.innerHTML = 'Display Author'
+        // your code here
 }
 
 // more codes for Search and Reset buttons here
