202bf47b81c98371d4bec3add8f70bdcf83e299e
play.js <git@gituser.com>
2021-08-04 23:12:15 +0700

    updated

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const data2 = '630612105'
 btn_toggle.onclick = () => {
   // your code here
   if(num==false){
-    const number = 630612104;
+    const number = 630612105;
     const number2 = parseInt(length.value);
     const number3 = number + number2;
     author.innerHTML = number3;
